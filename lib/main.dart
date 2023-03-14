548b6af65abc3cd8188bebe7744485ec2ecd5af1
gabriel-079 <contatogc871@gmail.com>
2023-03-14 11:31:13 -0300

    segunda versao

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen_and_ogin_page/pages/login_page.dart';
+import 'package:splash_screen_and_ogin_page/pages/splash_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: ('Demo'),
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        initialRoute: '/splash',
+        routes: {
+          '/splash': (_) => const SplashPage(),
+          '/login': (_) => const LoginPage(),
+        });
+  }
 }
